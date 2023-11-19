9cfc4da33d50021245ea2a3949e784380aa12ab8
Zayden ❤'s Life <96372751+zcraftelite9495@users.noreply.github.com>
2023-11-18 20:48:22 -0500

    Learning Python 2 - Finished Lesson 4

---
+++ Python/Personal Project 2024/Lessons/04 - Handling Errors.py
@@ -0,0 +1,7 @@
+# The original code was this
+# print("How do you make a hot dog stand?')
+# print(You take away its chair!)
+
+# My fixed code is here
+print("How do you make a hot dog stand?")
+print("You take away its chair!")
