245eaae585b80cce4113926be2c268da09b26ad1
Zayden ❤'s Life <96372751+zcraftelite9495@users.noreply.github.com>
2023-11-18 21:22:51 -0500

    Learning Python 2 - Finished Lesson 8

---
+++ Python/Personal Project 2024/Lessons/08 - Comments.py
@@ -0,0 +1,5 @@
+# Comments are a very basic thing in all coding languages, ive been using them all along LOL
+# Most likely declares the name of a city
+city_name = "St. Potatosburg"
+# Most likely declares the population of that city
+city_pop = 34000
