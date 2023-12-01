664ed331c5bbcc40aa071bd67928aac4d3d00a58
Zayden ❤'s Life <96372751+zcraftelite9495@users.noreply.github.com>
2023-12-01 11:42:41 -0500

    Quatro Tetris - Working on Gametest

--- Python/Personal Project 2024/Quatro-Tetris/quatro-tetris-gametest.py
+++ Python/Personal Project 2024/Quatro-Tetris/quatro-tetris-gametest.py
@@ -3,26 +3,50 @@
 # Import the modules used in the code
 import pygame
 
+# Import pygame.locals to easily access key coordinates
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+# Sets the screen Width & Height
+SCREEN_WIDTH=600
+SCREEN_HEIGHT=800
+
 # Initializes the pygame library, used in this code for displaying graphics on the game screen
 pygame.init()
 
 # Sets up the game's window size and draw area
-screen = pygame.display.set_mode([500,500])
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 # Script that makes sure the game runs until the user quits the game
 running = True
 while running:
 
-    # Detects if the user closes the window
     for event in pygame.event.get():
+        #  Detects and quits if the user closes the window
         if event.type == pygame.QUIT:
             running = False
+
+        # Detects and quits if the user hits the escape key
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
 
     # Fills the screen backround with black
     screen.fill((0, 0, 0))
 
-    # Flips the display so it displays correctly
-    pygame.display.flip()
+    # Create a surface and defines its length and width
+    surf = pygame.surface((50, 50))
+
+    # Gives the surface a color to sepeerate it from the backround
+    surf.fill((255, 255, 255))
+    rect = surf.get_rect()
 
 # Quits the game
 pygame.quit()
