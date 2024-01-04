ca3f4d61f4da4a21be8a22953d5c96a4555f60f5
Zayden ❤'s Life <96372751+zcraftelite9495@users.noreply.github.com>
2024-01-03 19:50:31 -0500

    Quatro Tetris/Personal Project 2024 - Original Tetris - Game is now a dark blue window :3

--- Python/Personal Project 2024/Quatro-Tetris/quatro-tetris-original.py
+++ Python/Personal Project 2024/Quatro-Tetris/quatro-tetris-original.py
@@ -1,8 +1,32 @@
 import pygame # Imports the Pygame Community Edition Modules
+import sys # Imports the Python Internal System Extension Modules
 
 pygame.init() # Initializes the 'pygame' module
 
+# Initializing display window
 screen = pygame.display.set_mode((300,600)) # Sets the display width and hight using ((W,H))
-pygame.display.set_caption("Quatro-Tetris: Original Tetris")
+pygame.display.set_caption("Quatro-Tetris: Original Tetris") # Sets the game's window title
 
-clock = pygame.time.Clock() # Sets the frame rate of the game (how fast the game will run), make sure to use capital C in 'Clock'
+# Initializing shortcuts to commands
+clock = pygame.time.Clock() 
+
+# Initializng Colors
+dark_blue = (44, 44, 127)
+
+while True: # Initiates the main game loop
+    # Checking for Events
+    ## Checks for events to stop the game loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # Detects if the 'x' button is pressed, and runs the following commands
+            pygame.quit() # Closes the game
+            sys.exit() # Exits the python interpreter
+    
+    # Drawing objects
+    screen.fill(dark_blue)
+    pygame.display.update() # Updates all objects on the screen 
+    clock.tick(60) # Sets the framerate of the game, to keep it running consistently
+
+    # Updating position of objects
+
+    # Drawing new position of objects
+    
