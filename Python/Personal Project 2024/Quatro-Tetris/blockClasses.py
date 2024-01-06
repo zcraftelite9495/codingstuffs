59714418f6989724a0bef6c3ebe11814825c4290
Zayden ❤'s Life <96372751+zcraftelite9495@users.noreply.github.com>
2024-01-06 17:35:10 -0500

    Quatro Tetris/Personal Project 2024 - Original Tetris - Debugging error with setting the L_Block's ID

--- Python/Personal Project 2024/Quatro-Tetris/blockClasses.py
+++ Python/Personal Project 2024/Quatro-Tetris/blockClasses.py
@@ -3,7 +3,7 @@ from classes import Position # Ensure the Position class is available for use in
 
 class L_Block(Block): # Creates a class for the L Tertromino
     def __init__(self): 
-        super().__init__(id = 1)
+        super().__init__(1) # Initializes the parent class' data and sets the ID to 1
         self.cells = { # Defines rotation states' cell positions
             0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
             # [0][0][1]
